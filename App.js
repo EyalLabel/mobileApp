105062de80dfa16c617366acb7d6fde57bc2e34f
Eyal Label <sslabel2@gmail.com>
2021-12-14 15:54:58 +0200

    commit

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  let x=29;
+  console.log("successful");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>I did it mom!</Text>
       <StatusBar style="auto" />
     </View>
   );
