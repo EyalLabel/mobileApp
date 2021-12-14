a9191cb8a6bdcdfd443f4211d1b5f80a2f441f28
Eyal Label <sslabel2@gmail.com>
2021-12-14 17:28:20 +0200

    commit

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View ,SafeAreaView,Image} from 'react-native';
-
+import { StyleSheet, Text, View ,SafeAreaView,Image,TouchableWithoutFeedback} from 'react-native';
+import {
+  useFonts,
+  AbhayaLibre_400Regular,
+  AbhayaLibre_500Medium,
+  AbhayaLibre_600SemiBold,
+  AbhayaLibre_700Bold,
+  AbhayaLibre_800ExtraBold,
+} from '@expo-google-fonts/abhaya-libre';
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    AbhayaLibre_400Regular,
+    AbhayaLibre_500Medium,
+    AbhayaLibre_600SemiBold,
+    AbhayaLibre_700Bold,
+    AbhayaLibre_800ExtraBold,
+  });
   let x=29;
   console.log("successful");
   return (
+   
     <SafeAreaView style={styles.container}>
-      <Text>I did it mom!</Text>
-      <Image source={require("./assets/icon.png")}/>
+       <View style={styles.topper}>
+      <Text style={styles.titleText}>My Plants</Text>
+      <Image style={styles.plantIcon} source={require("./assets/plantIcon.png")}/>
+    </View>
+      <Text>I did it mom!</Text> 
+      <TouchableWithoutFeedback onPress={()=>console.log("puppy")}> 
+      <Image resizeMode="cover"
+      source={{
+        width:250,
+        height:300,
+        uri:"https://picsum.photos/id/237/200/300"}}/>
+        </TouchableWithoutFeedback>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -21,4 +46,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  topper:{
+    position: 'absolute',
+    width:414,height:97,left:0,top:69,
+  },
+  titleText:{
+    fontFamily: 'AbhayaLibre_400Regular',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: 40,
+    lineHeight: 40,
+    left:24,
+    color:'#9AB0A6'
+  },
+  plantIcon:{
+    position:'absolute',
+width: 47,
+height: 47,
+left: 300,
+top: 0
+  }
+  
 });
